a8f2352435277a1e52cc77a3dbcb24ee2f30bf21
furiouscarl12 <79508407+furiouscarl12@users.noreply.github.com>
2022-12-15 20:30:11 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,134 @@
+from geopy import distance
+import mysql.connector
+import math
+import time
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='game',
+    user='database',
+    password='password',
+    autocommit=False
+)
+
+airports = []
+flight_compare = ""
+temp_dest = ""
+turns_total = 0
+km_total = 0
+dist_by_type = {
+    "heliport": 100,
+    "small_airport": 300,
+    "medium_airport": 500,
+    "seaplane_base": 100,
+    "large_airport": 1000,
+
+}
+
+
+def get_distance(curr_lat, curr_long, dest_lat, dest_long):
+    distance_result = distance.distance([curr_lat, curr_long],
+                                        [dest_lat, dest_long]).km
+    return distance_result
+
+
+def generate_random_location():
+    sql = "SELECT ident, airport.name as airport_name," \
+          "country.name as country_name, type, latitude_deg, longitude_deg " \
+          "FROM airport, country WHERE NOT type='closed' " \
+          "and airport.iso_country = country.iso_country" \
+          " ORDER BY RAND() LIMIT 1;"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+
+
+def fetch_available_airports(curr_lat, curr_long, type):
+    if type in dist_by_type:
+        radius_km = dist_by_type[type]
+    else:
+        raise Exception(f"Airport type '{type}' is invalid.")
+
+
+    sql = f"""SELECT ident, airport.name, country.name, type, latitude_deg, longitude_deg FROM airport, country
+    WHERE 3963.0 * acos((sin(RADIANS({curr_lat})) * sin(RADIANS(latitude_deg))) +
+    cos(RADIANS({curr_lat})) * cos(RADIANS(latitude_deg)) *
+    cos(RADIANS(longitude_deg) - RADIANS({curr_long}))) * 1.609344 <= {radius_km}
+    AND type != 'closed'
+    AND ident != '{curr["ident"]}'
+    AND country.iso_country = airport.iso_country;"""
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    airports.clear()
+
+
+
+def print_available_airports():
+    global dest
+    for i, airport in enumerate(airports):
+        if airport['ident'] == dest['ident']:
+            print("Oma päämäärä")
+
+        print(f"{str(i + 1) + ':':<5} {airport['airport_name'][0:43] + ',':<45}"
+              f" {airport['type']:<20} {airport['country_name'][0:23]:<25}"
+              f" {str(round(get_distance(curr['lat'], curr['long'], airport['lat'], airport['long']), 2)) + ' km':<15}"
+              f" {airport['direction']}")
+
+def print_starting_message():
+    print(
+          f"\n{'Pienistä voit lentää':<35} {dist_by_type['small_airport']} km"
+          f"\n{'Keskikokoisista lentokentistä voit lentää':<35} {dist_by_type['medium_airport']} km"
+          f"\n{'Isot lentokentät':<35} {dist_by_type['large_airport']} km\n"
+          "\n")
+
+
+def move(index, flight):
+    global curr, turns_total, km_total
+    turns_total = turns_total + 1
+    km_total = km_total + flight
+    curr = airports[index]
+
+
+
+
+
+
+
+
+curr = generate_random_location()
+dest = generate_random_location()
+dist = get_distance(curr["lat"], curr["long"], dest["lat"], dest["long"])
+while curr['ident'] != dest['ident']:
+    print(f"\nNykyinen siainti '{curr['airport_name']}', {curr['type']} in {curr['country_name']}"
+          f"\nPäämääräsi on '{dest['airport_name']}' in {dest['country_name']}."
+          f"\npäämäärä on {dist:.0f} km päässä.")
+while dest == curr or (dist > 4000 or dist < 1500):
+    dest = generate_random_location()
+    dist = get_distance(curr["lat"], curr["long"], dest["lat"], dest["long"])
+
+
+
+
+
+    input("\nPaina enter jotta voit löytää saatavat lentokentät.")
+    fetch_available_airports(curr["lat"], curr["long"], curr["type"])
+    print_available_airports()
+
+
+    index = input("\nKirjoita määränpään index: ")
+    while not index.isdigit() or (int(index) >= len(airports) + 1 or int(index) < 1):
+
+    airport_dist = get_distance(curr['lat'], curr['long'], airports[index]['lat'], airports[index]['long'])
+    dest_dist = get_distance(curr["lat"], curr["long"], dest["lat"], dest["long"])
+
+
+    if airport_dist > dest_dist:
+        dist *= -1
+
+    move(index, airport_dist)
+
+##print_results()
+connection.close()
---
+++ main.js
@@ -0,0 +1,114 @@
+"use strict";
+
+const currNameTag = document.getElementById('name');
+const currCountryTag = document.getElementById('current country');
+const destNameTag = document.getElementById('name');
+const destCountryTag = document.getElementById('country');
+const turnTag = document.getElementById('turn');
+const airportsTag = document.getElementById('airports');
+const map = L.map('map').setView([0, 0], 2);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+
+async function main() {
+    const dest = await fetchTimes('dest', 3, 'destination');
+
+    console.log('Destination:');
+    console.dir(dest);
+
+    destNameTag.innerText = dest['airport_name'];
+    destCountryTag.innerText = dest['country_name'];
+
+
+    const currentData = await fetchTimes('current', 3, 'current data');
+    if (!currentData || Object.keys(currentData).length === 0) { return; }
+
+    console.log('Current data:');
+    console.dir(currentData);
+
+    let {'current': curr, airports, dist, turn, 'total_km': totalKm,'total_co2': totalCO2} = currentData;
+
+    updateHeader(curr, turn, totalKm, totalCO2);
+    updateAirportsList(airports);
+
+    map.panTo(([curr['lat'], curr['long']]));
+
+    const markerCurr = L.marker([curr['lat'], curr['long']]).addTo(map);
+    const markerDest = L.marker([dest['lat'], dest['long']]).addTo(map);
+
+    airportsTag.addEventListener('mouseover', (event) => {
+        map.zoomIn(3);
+    }, {once : true});
+}
+
+main();
+
+
+function fetchFrom(url, resource) {
+    const promise = fetch(String(url))
+        .then((response) => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error(`Failed to fetch ${String(resource)}. Error: ${error.message}`);
+        });
+    return promise;
+
+
+}
+
+function updateAirportsList(airports) {
+    const frag = new DocumentFragment;
+    const liItems = [];
+    let marker, markersel;
+
+function updateHeader(curr, turn, totalKm,) {
+    currNameTag.innerText = curr['airport_name'];
+    turnTag.innerText = ` ${turn}`;
+    currCountryTag.innerText = curr['country_name'];
+
+
+    Array.from(airports).forEach((airport) => {
+        const name = document.createElement('span');
+        const country = document.createElement('span');
+        const direction = document.createElement('span');
+        const type = document.createElement('span');
+        const dist = document.createElement('span');
+
+        name.classList.add('airport-name');
+        type.classList.add('airport-type');
+        country.classList.add('airport-country');
+        direction.classList.add('airport-direction');
+        dist.classList.add('airport-dist');
+
+        name.innerText = airport['airport_name'];
+        type.innerText = airport['type'].split('_').join(' ');
+        country.innerText = 'in ' + airport['country_name'] + ',';
+        direction.innerText = 'in ' + airport['direction'] + ' direction,';
+        dist.innerText = String(Math.round(airport['distance'])) + ' km päässä.';
+
+        li.addEventListener('mouseover', (event) => {
+            marker = L.marker([airport['lat'], airport['long']]).addTo(map);
+            marker._icon.style.filter = "hue-rotate(200deg)";
+        });
+
+        li.addEventListener('mouseout', (event) => {
+            marker.remove();
+        });
+
+
+            if (markersel) {
+                markersel.remove();
+            }
+
+        });
+
+        li.append(name, type, country, direction, dist, co2);
+        liItems.push(li);
+        frag.append(li);}}
+
